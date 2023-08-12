4e434c5a63a9bc2341a645d694fae58a49b56d21
Frank Plowman <post@frankplowman.com>
2023-08-12 17:50:28 +0100

    Update site metadata
    * Change description
    * Add `og:description` tag

--- components/layout.js
+++ components/layout.js
@@ -2,25 +2,24 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const name = 'Frank Plowman';
-export const siteTitle = name;
+const author = 'Frank Plowman';
+export const title = author;
+const description = 'Engineer interested in multimedia and embedded systems';
 
 export default function Layout({ children, home }) {
   return (
     <div>
       <Head>
         <link rel="icon" href="data:," />
-        <meta
-          name="description"
-          content="Personal site for an engineer interested in multimedia and embedded systems"
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="author" content={siteTitle} />
+        <meta name="description" content={description} key="desc" />
+        <meta name="author" content={author} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
       </Head>
       <header>
         {
           <>
-            <h1><Link href={`/`}>{name}</Link></h1>
+            <h1><Link href={`/`}>{title}</Link></h1>
           </>
         }
       </header>
